90771d9d2d17b6dd0dab656e207fc4c5dbdb33c7
anasbouabid <bouabidanas02@gmail.com>
2023-01-20 18:06:23 +0100

    renamed some endpoints

--- src/main/java/com/anasbouabid/centre_dentaire/controllers/DiseaseController.java
+++ src/main/java/com/anasbouabid/centre_dentaire/controllers/DiseaseController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/disease")
+@RequestMapping("/api/diseases")
 public class DiseaseController {
     private DiseaseServiceImpl diseaseService;
 
